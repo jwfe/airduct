9be8d3f9bb9b57f5e6fee45d72633f666003444e
刘卿 <lqing@jointwisdom.cn>
2018-12-11 14:26:32 +0800

    fixed: 依赖更新后，需要install

--- libs/update.js
+++ libs/update.js
@@ -11,7 +11,7 @@ module.exports = function(){
         return;
     }
     
-    shell.echo('[1/4]准备更新配置文件');
+    shell.echo('[1/5]准备更新配置文件');
     var pwd = shell.pwd().stdout;
     var airduct = require(path.resolve(pwd, "airduct.config"));
     shell.cd(`../`);
@@ -19,7 +19,7 @@ module.exports = function(){
     shell.exec('git clone ' + airduct.public.git + ' '+ tempDir);
     shell.cd(pwd);
 
-    shell.echo('[2/4]下载完成准备覆盖原配置');
+    shell.echo('[2/5]下载完成准备覆盖原配置');
     var tempAbsDir = path.join(pwd, '../' + tempDir);
     var files = glob.sync(path.resolve(tempAbsDir, '*.{js,json}'));
     files.forEach((item) => {
@@ -27,11 +27,14 @@ module.exports = function(){
     });
     shell.rm('-rf', tempAbsDir)
 
-    shell.echo('[3/4]配置更新完成，准备重启webpack');
+    shell.echo('[3/5]配置更新完成，准备重启webpack');
     var whoami = shell.exec('who am i').stdout;
     whoami = whoami.split(' ')[0];
     shell.exec('ps aux | grep "' + whoami + '" | grep "webpack" |grep -v grep | cut -c 9-15 | xargs kill -9');
 
-    shell.echo('[4/4]杀掉webpack完成，准备重启');
+    shell.echo('[4/5]配置更新完成，准备重启webpack');
+    shell.exec(`npm install`);
+
+    shell.echo('[5/5]杀掉webpack完成，准备重启');
     shell.exec(`npm run dev`);
 }
